aaf2fd1a0d2ddbc288847c39e767a4cdbdc6dfea
johndward01 <johndward01@gmail.com>
2021-09-02 11:51:28 -0500

    Add project files.

---
+++ Pokedex Demo/Pokemon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pokedex_Demo
+{
+    class Pokemon
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
---
+++ Pokedex Demo/Program.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Text.Json;
+
+namespace Pokedex_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // read the json file into a string
+            var pokeFile = File.ReadAllText(@"C:\Users\johnd\Desktop\Repo\Pokedex Demo\Pokedex Demo\pokedex.json");
+
+            var parsedPokeFile = JObject.Parse(pokeFile).GetValue("pokemon");
+
+            foreach (var item in parsedPokeFile)
+            {
+                var info = JsonSerializer.Deserialize<Pokemon>(item.ToString());
+                Console.WriteLine(info.name);
+            }
+
+        }
+    }
+}
